89b74008b867baebd75dbcfa06ef01d3554e43f1
Robert Schaedler III <robertschaedler3@gmail.com>
2020-08-24 16:38:53 -0400

    add tests

--- homework/hw01.py
+++ homework/hw01.py
@@ -1,4 +1,13 @@
+"""
+This file shows some simple python code to solve the Triangles assignment.   
+The primary goal of this file is to demonstrate a simple python program and the use of the
+unittest package.
+
+@author: robertschaedler3
+"""
+
 import sys
+import unittest
 
 
 def classify_triangle(a, b, c):
@@ -26,8 +35,37 @@ def classify_triangle(a, b, c):
         return 'NotATriangle'
 
 
+class TestTriangles(unittest.TestCase):
+
+    def testEquilateral(self):
+        self.assertEqual(classify_triangle(1, 1, 1),
+                         'Equilateral', '1,1,1 is an equilateral triangle')
+
+    def testIsosceles(self):
+        self.assertEqual(classify_triangle(1, 2, 2),
+                         'Isosceles', '1,2,2 is an isosceles triangle')
+        self.assertEqual(classify_triangle(2, 1, 2),
+                         'Isosceles', '2,1,2 is an isosceles triangle')
+        self.assertEqual(classify_triangle(2, 2, 1),
+                         'Isosceles', '2,2,1 is an isosceles triangle')
+
+    def testRight(self):
+        self.assertEqual(classify_triangle(3, 4, 5), 'Right',
+                         '3,4,5 is a Right triangle')
+
+    def testScalene(self):
+        self.assertEqual(classify_triangle(5, 7, 9), 'Scalene',
+                         '5,7,9 is a Right triangle')
+
+    def testNotATriangle(self):
+        self.assertEqual(classify_triangle(1, 1, 100), 'NotATriangle',
+                         '1,1,100 is not a triangle')
+
+
 if __name__ == '__main__':
-    if len(sys.argv) != 4:
+    if len(sys.argv) == 1:
+        unittest.main(exit=False)
+    elif len(sys.argv) != 4:
         print('Usage: python .\hw01.py <length A> <length B> <length C>')
     else:
         try:
