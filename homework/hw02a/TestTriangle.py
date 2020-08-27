d950962c667ae3e6fed8c9c11aacab79935fe4af
Robert Schaedler III <robertschaedler3@gmail.com>
2020-08-27 15:57:45 -0400

    create additional triangle tests

--- homework/hw02a/TestTriangle.py
+++ homework/hw02a/TestTriangle.py
@@ -1,32 +1,51 @@
 # -*- coding: utf-8 -*-
 """
-Updated Jan 21, 2018
+Updated Aug 27, 2020
 The primary goal of this file is to demonstrate a simple unittest implementation
 
-@author: jrr
-@author: rk
+@author: robertschaedler3
 """
 
 import unittest
+from itertools import permutations
 
 from Triangle import classifyTriangle
 
 # This code implements the unit test functionality
 # https://docs.python.org/3/library/unittest.html has a nice description of the framework
 
+
 class TestTriangles(unittest.TestCase):
-    # define multiple sets of tests as functions with names that begin
 
-    def testRightTriangleA(self): 
-        self.assertEqual(classifyTriangle(3,4,5),'Right','3,4,5 is a Right triangle')
+    def testRightTriangle(self):
+        for triangle in list(permutations([3, 4, 5])):
+            triangle = list(triangle)
+            self.assertEqual(classifyTriangle(*triangle), 'Right',
+                             f'{triangle} is a Right triangle')
 
-    def testRightTriangleB(self): 
-        self.assertEqual(classifyTriangle(5,3,4),'Right','5,3,4 is a Right triangle')
-        
-    def testEquilateralTriangles(self): 
-        self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
+    def testScaleneTriangle(self):
+        for triangle in list(permutations([2, 3, 4])):
+            triangle = list(triangle)
+            self.assertEqual(classifyTriangle(*triangle), 'Scalene',
+                             f'{triangle} is a Scalene triangle')
+
+    def testIscoscelesTriangle(self):
+        for triangle in list(permutations([1, 1, 2])):
+            triangle = list(triangle)
+            self.assertEqual(classifyTriangle(*triangle), 'Iscosceles',
+                             f'{triangle} is an Iscoseles triangle')
+
+    def testEquilateralTriangles(self):
+        self.assertEqual(classifyTriangle(1, 1, 1),
+                         'Equilateral', '1,1,1 is an Equilaterial triangle')
+
+    def testNotATriangle(self):
+        for triangle in list(permutations([1, 1, 100])):
+            triangle = list(triangle)
+            self.assertEqual(classifyTriangle(*triangle),
+                             'NotATriangle', f'{triangle} is not a triangle')
+
 
 if __name__ == '__main__':
     print('Running unit tests')
     unittest.main()
-
