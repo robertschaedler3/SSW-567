48c57b9f6b8aabb30356eb7a7b06b7b6310df4a5
Robert Schaedler III <robertschaedler3@gmail.com>
2020-09-30 12:23:11 -0400

    rename get_repos_json to get_repos

--- homework/hw04.py
+++ homework/hw04.py
@@ -5,7 +5,7 @@ import functools
 GITHUB_API = 'https://api.github.com'
 
 
-def get_repos_json(owner):
+def get_repos(owner):
     res = requests.get(f'{GITHUB_API}/users/{owner}/repos')
     return [repo['name'] for repo in res.json()]
 
@@ -22,7 +22,7 @@ if __name__ == '__main__':
 
     owner = sys.argv[1]
 
-    data = [(repo, get_commits(owner, repo)) for repo in get_repos_json(owner)]
+    data = [(repo, get_commits(owner, repo)) for repo in get_repos(owner)]
 
     # print results
     for repo, commit_count in data:
