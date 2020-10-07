a00227c1150f58a4c9fdf8f590f204763fc8c06e
Robert Schaedler III <robertschaedler3@gmail.com>
2020-10-07 15:56:53 -0400

    add mocking for github api requests

--- homework/test/test_hw04.py
+++ homework/test/test_hw04.py
@@ -1,20 +1,27 @@
 import unittest
+from unittest.mock import patch
 
-from hw04 import get_repos, get_commits
+import hw04
 
-# A known valid account name and repository
 OWNER = 'robertschaedler3'
 REPO = 'SSW-567'
+COMMITS = 100
 
 
 class TestHw4(unittest.TestCase):
 
-    def test_get_repos(self):
-        repos = get_repos(OWNER)
+    @patch('hw04.get_repos')
+    def test_get_repos(self, mock_get_repos):
+        mock_get_repos.return_value = [REPO]
+
+        repos = hw04.get_repos(OWNER)
         self.assertIsInstance(repos, list)
         for item in repos:
             self.assertIsInstance(item, str)
 
-    def test_get_commits(self):
-        commits = get_commits(OWNER, REPO)
+    @patch('hw04.get_commits')
+    def test_get_commits(self, mock_get_commits):
+        mock_get_commits.return_value = COMMITS
+
+        commits = hw04.get_commits(OWNER, REPO)
         self.assertIsInstance(commits, int)
